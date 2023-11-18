10c7ec17a7cdb803a78c1307852e42315d8d9379
ilnarisrisov <146421910+ilnarisrisov@users.noreply.github.com>
2023-11-18 22:22:34 +0500

    Исправил обработку ошибки натуральности числа

--- фт-220007, бочонки.py
+++ фт-220007, бочонки.py
@@ -6,10 +6,13 @@ logging.basicConfig(filename='barrels.log', level=logging.INFO)
 while True:
     try:
         N = int(input('Введите количество бочонков: '))
+
         if N < 1:
             print('Должно быть введено натуральное число(больше 0)')
             logging.error('Должно быть введено натуральное число(больше 0)')
-        break
+        else:
+            break
+
     except ValueError:
         print('Должно быть введено число')
         logging.error('Должно быть введено число')
@@ -20,7 +23,7 @@ random_sequence = list(range(1, N+1))
 random.shuffle(random_sequence)
 while i < N:
 
-    enter = input('\nНажмите Enter, чтобы вытащить бочку: {}'.format(random_sequence[i]))
+    enter = input('Нажмите Enter, чтобы вытащить бочку: {}'.format(random_sequence[i]))
     logging.info('Вытащен бочонок с номером {}'.format(random_sequence[i]))
     i += 1
 
